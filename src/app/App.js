7b7308f4fe84a17e9d84cdb3e4e107aabd45c503
minhsonle <info.leminhson@gmail.com>
2025-08-17 13:05:35 -0400

    Add login page and header styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,5 @@
 import '../App.css';
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Nav from "../components/Nav";
+
 import Body from "../components/Body";
 
 function App() {
